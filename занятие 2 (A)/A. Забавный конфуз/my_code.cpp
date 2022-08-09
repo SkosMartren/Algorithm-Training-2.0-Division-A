5f123d220e846a6d57f5a52617e3d79576d31ef6
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2022-08-09 15:35:56 +0300

    Create my_code.cpp

---
+++ занятие 2 (A)/A. Забавный конфуз/my_code.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm> // swap
+#include<cmath> // abs
+using namespace std;
+
+int main() {
+
+
+	int N, K;
+	cin >> N >> K;
+
+	int64_t B_max, B_min;
+	cin >> B_max >> B_min;
+
+	if (B_max < B_min) {
+		swap(B_max, B_min);
+	}
+
+	for (int i = 2; i < N; ++i) {
+		int B_i;
+		cin >> B_i;
+
+		if (B_max < B_i) {
+			B_max = B_i;
+		}
+
+		if (B_min > B_i) {
+			B_min = B_i;
+		}
+	}
+
+	cout << abs(B_max - B_min);
+	return 0;
+}
