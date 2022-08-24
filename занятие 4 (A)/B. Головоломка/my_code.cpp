8e49470dc4a60bd65d4106b7a1a31adafb3a7b88
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2022-08-24 16:50:50 +0300

    Update my_code.cpp

--- занятие 4 (A)/B. Головоломка/my_code.cpp
+++ занятие 4 (A)/B. Головоломка/my_code.cpp
@@ -3,26 +3,13 @@
 
 using namespace std;
 
-void change_count_sml_plus(unordered_map <char, int>& table_sml_to_count_sml, const int counter) {
+void change_count_sml(unordered_map <char, int>& table_sml_to_count_sml, const int counter, const int delta) {
 
 	for (int i = 0; i < counter; ++i) {
 		string word;
 		cin >> word;
 		for (const auto sml : word) {
-			++table_sml_to_count_sml[sml];
-		}
-
-	}
-
-}
-
-void change_count_sml_minus(unordered_map <char, int>& table_sml_to_count_sml, const int counter) {
-
-	for (int i = 0; i < counter; ++i) {
-		string word;
-		cin >> word;
-		for (const auto sml : word) {
-			--table_sml_to_count_sml[sml];
+			table_sml_to_count_sml[sml] = table_sml_to_count_sml[sml] + delta;
 		}
 
 	}
@@ -36,8 +23,8 @@ int main() {
 	cin >> N >> M;
 
 	unordered_map <char, int> table_sml_to_count_sml;
-	change_count_sml_plus(table_sml_to_count_sml, N);
-	change_count_sml_minus(table_sml_to_count_sml, M);
+	change_count_sml(table_sml_to_count_sml, N, +1);
+	change_count_sml(table_sml_to_count_sml, M, -1);
 
 	// output
 	for (const auto [sml, count] : table_sml_to_count_sml) {
